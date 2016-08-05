6079e81412d498636dfeb916530c0bf4b7358aee
Marcus Bernales <mboperator@gmail.com>
2016-08-05 15:01:15 -0700

    Heroku configuration

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,15 @@ use Mix.Config
 # which you typically run after static files are built.
 config :assassin_backend, AssassinBackend.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "assassins-staging.herokuapp.com", port: 443],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :assassin_backend, AssassinBackend.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
